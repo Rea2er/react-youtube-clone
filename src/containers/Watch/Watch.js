c80edb62a6b93e57c60b5a8666a2028916de1c9a
Ruize Nie <rz376775@dal.ca>
2020-08-04 20:03:20 -0300

    layout video detail page using css grid

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 import RelatedVideos from "../../components/RelatedVideos/RelatedVideos";
+import Video from "../../components/Video/Video";
 import "./Watch.scss";
 
 class Watch extends Component {
   render() {
-    return <RelatedVideos />;
+    return (
+      <div className="watch-grid">
+        <Video className="video" id="-7fuHEEmEjs" />
+        <div
+          className="metadata"
+          style={{ width: "100%", height: "100px", background: "#F91112" }}
+        >
+          Metadata
+        </div>
+        <div
+          className="video-info-box"
+          style={{ width: "100%", height: "100px", background: "#BD10E0" }}
+        >
+          Video Info box
+        </div>
+        <div
+          className="comments"
+          style={{ width: "100%", height: "100px", background: "#9013FE" }}
+        >
+          comments
+        </div>
+        <RelatedVideos className="relatedVideos" />
+      </div>
+    );
   }
 }
 
