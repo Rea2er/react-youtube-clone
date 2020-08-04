65b8db08bc3eb9e000689bc815b69b7d19954395
Ruize Nie <rz376775@dal.ca>
2020-08-04 14:48:55 -0300

    initial version of header bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import HeaderNav from "./containers/HeaderNav/HeaderNav";
+
 function App() {
-  return <div>Hello World</div>;
+  return <HeaderNav />;
 }
 
 export default App;
---
+++ src/containers/HeaderNav/HeaderNav.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+import { Image, Menu, Form, Input, Icon } from "semantic-ui-react";
+
+import logo from "../../assets/images/logo.png";
+import "./HeaderNav.scss";
+
+class HeaderNav extends Component {
+  render() {
+    return (
+      <Menu borderless className="top-menu" fixed="top">
+        <Menu.Item header className="logo">
+          <Image src={logo} size="tiny" />
+        </Menu.Item>
+        <Menu.Menu className="nav-container">
+          <Menu.Item className="search-input">
+            <Form>
+              <Form.Field>
+                <Input placeholder="Search" size="small" action="Go" />
+              </Form.Field>
+            </Form>
+          </Menu.Item>
+
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Icon className="header-icon" name="video camera" size="large" />
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="grid layout" size="large" />
+            </Menu.Item>
+            <Menu.Item>
+              <Icon className="header-icon" name="alarm" size="large" />
+            </Menu.Item>
+            <Menu.Item name="avatar">
+              <Image src="http://via.placeholder.com/80x80" avatar />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
+
+export default HeaderNav;
